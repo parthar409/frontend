eb262c743a6100462c268d74dbc4e40dc2ded4e8
parthar409 <parthar40888@gmail.com>
2024-10-22 02:08:15 +0600

    project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+    <div className="header-contents"></div>
+    <h2>Order Your Favourite food here</h2>
+    <p className='explore-menu-text'>Chose From a diverse menu featuring a delectable array of dishes crafted with  the finest INGREDIENT AND setisfy your craving and elevate your dining experience one delicious meal ata a time.</p>
+     <button>View Menu</button>
+  </div>
+  )
+}
+
+export default Header
